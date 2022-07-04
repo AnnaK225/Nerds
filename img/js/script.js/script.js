f0fc89cf2c88d4fcd0c099cb43fe5ce9c92400ad
Anna Kusnetsova <annakusnetsov@gmal.com>
2022-07-04 17:24:59 +0300

    add font

---
+++ img/js/script.js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.slider').slick({ 
+        arrows:false,
+        dots:true,
+        adaptiveHeigh:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:900,
+        easing:'ease',
+        infinite:false,
+        autoplaySpeed:1500,
+        autoplay: true,
+        infinite: false,
+        autoplaySpeed:3000,
+    });    
+});
+
